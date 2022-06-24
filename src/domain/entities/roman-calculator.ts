920687bb758b23ee0930e3123f457c081e43a58c
carol <carool@live.com>
2022-06-24 16:52:43 -0300

    feat: create entitie roman calculator

---
+++ src/domain/entities/roman-calculator.ts
@@ -0,0 +1,11 @@
+export class RomanCalculator {
+    constructor () {}
+
+    addition (number1: string, number2: string) {
+     // todo   
+    }
+
+    subtraction (number1: string, number2: string) {
+     // todo
+    }
+}
